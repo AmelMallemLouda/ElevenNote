8bf1101e5fbfccfb3e486f37b38a044f684954b5
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-01-30 15:20:32 -0500

    Challenge first commit

---
+++ ElevenNote.Models/CategoryCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+   public  class CategoryCreate
+    {
+        [Required]
+        [MinLength(4,ErrorMessage ="Please enter at least 4 characters.")]
+        [MaxLength(15,ErrorMessage ="There are tooo many caracters in this field, 15 characters is the maximum.")]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
---
+++ ElevenNote.Models/CategoryDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+   public  class CategoryDetails
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryType { get; set; }
+
+        [Display(Name = "Created")] // didn't change when displayed?!
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]// didn't change when displayed?!
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
