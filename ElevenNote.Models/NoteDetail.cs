29ccb9be18fe186d2ae9074dfce5145e36f56ebb
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-01-31 17:34:48 -0500

    Last Commit

--- ElevenNote.Models/NoteDetail.cs
+++ ElevenNote.Models/NoteDetail.cs
@@ -14,10 +14,10 @@ namespace ElevenNote.Models
         public string Content { get; set; }
 
         [Display(Name = "Created")]
-        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }// DateTimeOffset is reference type,Reference Types point to an address in memory
 
         [Display(Name = "Modified")]
-        public DateTimeOffset? ModifiedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }// the ?  is referred to as the null-conditional operator,in other words This allows the value type to be null 
         public string  CategoryName { get; set; }
     }
 }
